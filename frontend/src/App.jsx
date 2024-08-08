e3a21d0872a43c8f103825dd59d9d5d6931c2351
kwagley0 <121137026+kwagley0@users.noreply.github.com>
2024-08-08 12:40:31 -0400

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,14 +18,14 @@ const App = () => {
     fetchPlayers();
   }, []);
 
-  const fetchPlayers = async () => {
-    try {
-      const response = await axios.get("https://premier-zone.onrender.com/api/v1/player");
-      setPlayers(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+const fetchPlayers = async () => {
+  try {
+    const response = await axios.get("/api/v1/player");
+    setPlayers(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
   useEffect(() => {
     console.log(players);
