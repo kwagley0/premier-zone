26461b18a2d6b2d9126e1c2afaf52d30722f3781
kwagley0 <121137026+kwagley0@users.noreply.github.com>
2024-08-07 23:10:07 -0400

    fix

--- backend/src/main/java/com/pl/premier_zone/CorsConfig.java
+++ backend/src/main/java/com/pl/premier_zone/CorsConfig.java
@@ -7,14 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("https://premier-zone.vercel.app/:1")
-                        .allowedMethods("GET");
+                registry.addMapping("/**")
+                        .allowedOrigins("https://premier-zone.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
--- backend/src/main/java/com/pl/premier_zone/player/PlayerController.java
+++ backend/src/main/java/com/pl/premier_zone/player/PlayerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("https://premier-zone.vercel.app/:1")
+// @CrossOrigin("https://premier-zone.vercel.app") 
 @RequestMapping(path = "api/v1/player")
 public class PlayerController {
     private final PlayerService playerService;
