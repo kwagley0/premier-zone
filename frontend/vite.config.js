740f00ca869beeabf8c265bccbfd75b749153688
kwagley0 <121137026+kwagley0@users.noreply.github.com>
2024-08-07 22:12:39 -0400

    CORS Fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://premier-zone.onrender.com/api/v1/player',
+      },
+    },
+  }
 });
