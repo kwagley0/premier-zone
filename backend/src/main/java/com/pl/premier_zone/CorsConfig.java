aa6c386a1c1078c8274535cddb488434f8e4f1f2
kwagley0 <121137026+kwagley0@users.noreply.github.com>
2024-08-09 19:03:13 -0400

    fix

--- backend/src/main/java/com/pl/premier_zone/CorsConfig.java
+++ backend/src/main/java/com/pl/premier_zone/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.pl.premier_zone;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
