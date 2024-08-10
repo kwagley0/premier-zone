52dff70e804297fcf7e5942080dc08a988384d24
kwagley0 <121137026+kwagley0@users.noreply.github.com>
2024-08-09 20:21:53 -0400

    fix

--- backend/src/main/java/com/pl/premier_zone/player/PlayerController.java
+++ backend/src/main/java/com/pl/premier_zone/player/PlayerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://premier-zone.vercel.app"})
 @RequestMapping(path = "api/v1/player")
 public class PlayerController {
     private final PlayerService playerService;
