19bb7c18ab50393afaeff09797ad0a24afd34968
kwagley0 <121137026+kwagley0@users.noreply.github.com>
2024-11-02 18:27:35 -0400

    Added CR7 image to homepage

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import { Link } from 'react-router-dom';
 import LogoPL from '../../assets/images/PL.webp';
+import CR7 from "../../assets/images/CR7.png";
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
@@ -20,24 +21,37 @@ const Home = () => {
         };
       }, []);
 
-    return(
+    return (
       <>
-        <div className = "container home-page">
-            <div className="text-zone">
-                <h1>
-                <img src={LogoPL} alt = "PremierZone" />
-                <br />
-                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={12} />
-                <br /> 
-                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15} /> 
-                </h1>
-                <h2>Your home for everything Premier League related!</h2>
-                <Link to="/teams" className="flat-button">GET STARTED</Link>
-            </div>
+        <div className="container home-page">
+          <div className="text-zone">
+            <h1>
+              <img src={LogoPL} alt="PremierZone" />
+              <br />
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={nameArray}
+                idx={12}
+              />
+              <br />
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={jobArray}
+                idx={15}
+              />
+            </h1>
+            <h2>Your home for everything Premier League related!</h2>
+            <Link to="/teams" className="flat-button">
+              GET STARTED
+            </Link>
+          </div>
+          <div className="image-zone">
+            <img src={CR7} alt=""/>
+          </div>
         </div>
         <Loader type="pacman" />
       </>
-    )
+    );
 }
 
 export default Home
